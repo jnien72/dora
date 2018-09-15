699331135173ea6d0b21b4ce6ef53c63cefa7457
johnny <a@b.com>
2018-09-15 11:51:29 +0800

    fix webconsole startup script

--- sbin/start-web-console.sh
+++ sbin/start-web-console.sh
@@ -19,6 +19,7 @@ done;
 CLASSPATH="etc:$CLASSPATH";
 
 JVM_OPTS="$JVM_OPTS -Dfile.encoding=UTF-8";
+JVM_OPTS="$JVM_OPTS -verbose:gc";
 JVM_OPTS="$JVM_OPTS -XX:+UseG1GC"
 JVM_OPTS="$JVM_OPTS -XX:MaxGCPauseMillis=20"
 JVM_OPTS="$JVM_OPTS -XX:InitiatingHeapOccupancyPercent=35"
@@ -26,12 +27,21 @@ JVM_OPTS="$JVM_OPTS -XX:+PrintGCDateStamps";
 JVM_OPTS="$JVM_OPTS -XX:+PrintHeapAtGC";
 JVM_OPTS="$JVM_OPTS -XX:+PrintGCDetails";
 JVM_OPTS="$JVM_OPTS -XX:+PrintTenuringDistribution";
+JVM_OPTS="$JVM_OPTS -XX:+UseGCLogFileRotation";
+JVM_OPTS="$JVM_OPTS -XX:NumberOfGCLogFiles=1";
+JVM_OPTS="$JVM_OPTS -XX:GCLogFileSize=1M";
 JVM_OPTS="$JVM_OPTS -XX:+AlwaysPreTouch";
 JVM_OPTS="$JVM_OPTS -XX:+UseCompressedOops";
-JVM_OPTS="$JVM_OPTS -Xmx128m"
-JVM_OPTS="$JVM_OPTS -Xms128m"
+JVM_OPTS="$JVM_OPTS -XX:+HeapDumpOnOutOfMemoryError";
+JVM_OPTS="$JVM_OPTS -XX:HeapDumpPath=logs/web-server-heap-dump.hprof";
+JVM_OPTS="$JVM_OPTS -XX:ErrorFile=logs/web-server-java-error.log";
+JVM_OPTS="$JVM_OPTS -Xloggc:logs/web-server-gc.log";
+JVM_OPTS="$JVM_OPTS -Xmx256m"
+JVM_OPTS="$JVM_OPTS -Xms256m"
+JVM_OPTS="$JVM_OPTS -Dhdp.version=2.4.2.0"
 
 pid=pid/web-console.pid
+log=logs/web-console.log
 
 if [ -f $pid ]; then
       if kill -0 `cat $pid` > /dev/null 2>&1; then
@@ -40,6 +50,7 @@ if [ -f $pid ]; then
       fi
 fi
 
-echo starting server
-nohup java -classpath $CLASSPATH $JVM_OPTS com.eds.dora.web.WebConsole "$@" &
+echo starting server, sending stdout/stderr to $log
+nohup java -classpath $CLASSPATH $JVM_OPTS com.eds.dora.web.WebConsole "$@" > "$log" 2>&1 < /dev/null &
 echo $! > $pid
+sleep 1; head "$log"
